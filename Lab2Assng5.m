4c99ea47dbc9262810e17186f0b3782c3ebcb93b
poshposh <sopo.chikvinidze@gmail.com>
2016-09-25 02:04:56 +0200

    Add files via upload

--- Lab2Assng5.m
+++ Lab2Assng5.m
@@ -8,11 +8,15 @@ S2 = [2 0; 0 1];
 det1 = det(S1);
 %determinant of S2
 det2 = det(S2);
-disp(det1);
-disp(det2);
 %inverse of S1
 S1inv = inv(S1);
-disp(S1inv);
 %inverse of S2
 S2inv = inv(S2);
-disp(S2inv);
+%computing the decision boundary
+syms x
+syms y
+X = [x;y];
+g1=log(1/(2*pi*sqrt(det1))*exp((-1)/2*(X-m1)'/S1*(X-m1)))+log(0.3);
+g2=log(1/(2*pi*sqrt(det2))*exp((-1)/2*(X-m2)'/S2*(x-m2)))+log(0.7);
+solve(g1-g2==0, 'Real', true)
+
